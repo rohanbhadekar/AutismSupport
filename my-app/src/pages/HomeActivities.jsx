c93b1ddcf2da65c93ce094aa9c8c99f47246e93b
rohanbhadekar <rohan.bhadekar@gmail.com>
2025-07-28 22:20:38 +0530

    SiteMap

--- my-app/src/pages/HomeActivities.jsx
+++ my-app/src/pages/HomeActivities.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-
+import { Helmet } from "react-helmet";
+<Helmet>
+  <title>Parenting Autism Together | Support for Indian Parents</title>
+  <meta name="description" content="Explore resources, activities, and government schemes for autism parenting in India. Available in Marathi, Hindi, and English." />
+  <meta name="keywords" content="Autism, Parenting, India, Activities, Government Schemes, Marathi, Hindi, English" />
+  <link rel="canonical" href="https://parentingautismtogether.in/" />
+</Helmet>
 
 const HomeActivities = () => {
 
