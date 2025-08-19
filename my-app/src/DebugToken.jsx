78d9cb3ee0f3957d223fbc85bb3edabba09b5b98
rohanbhadekar <rohan.bhadekar@gmail.com>
2025-08-19 16:50:27 +0530

    Update DebugToken.jsx

--- my-app/src/DebugToken.jsx
+++ my-app/src/DebugToken.jsx
@@ -8,7 +8,7 @@ export default function CallApi() {
     const token = await getToken(); // default = 'session' token
     alert(`Token: ${token}`);
     console.log(`Token: ${token}`);
-    const res = await fetch("http://parentingautismtogether.up.railway.app/api/protected", {
+    const res = await fetch("https://parentingautismtogether.up.railway.app/api/protected", {
       headers: { Authorization: `Bearer ${token}` }
     });
     alert(await res.text());
