664cc72e965231254ace65092caaff6c7e03041d
rohanbhadekar <rohan.bhadekar@gmail.com>
2025-08-23 14:54:14 +0530

    Update index.mjs

--- api-server/index.mjs
+++ api-server/index.mjs
@@ -362,6 +362,8 @@ app.get('/ping', (req, res) => {
   console.log('✅ DB Host:', process.env.DB_HOST);
   console.log('✅ DB Port:', process.env.DB_PORT);
   console.log('✅ DB User:', process.env.DB_USER);
+  console.log('✅ NODE_ENV:', process.env.NODE_ENV);
+
 });
 
 
